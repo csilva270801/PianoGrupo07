0bc8a081f5f3d1b58d265d308cbcbbb75f3b1695
Josue Alvarado <josue.jdam12@gmail.com>
2023-10-07 10:37:22 -0600

    piano2y3

---
+++ app/src/main/java/com/example/pianogrupo07/piano_animales.java
@@ -0,0 +1,14 @@
+package com.example.pianogrupo07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class piano_animales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piano_animales);
+    }
+}
